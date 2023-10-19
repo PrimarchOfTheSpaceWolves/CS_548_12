f1a522848e5c19735717cbb5eb16a5ae1c8e52ba
DMR <realemj@sunypoly.edu>
2023-10-19 16:08:11 -0400

    Modified CG

--- CG/train.py
+++ CG/train.py
@@ -23,6 +23,7 @@ from options.train_options import TrainOptions
 from data import create_dataset
 from models import create_model
 from util.visualizer import Visualizer
+import os
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
